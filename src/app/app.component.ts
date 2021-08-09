008e0a2ea1b9cbb2c45ca5708b0d1620bdff96bb
ManuDev03 <manuprajan03@gmail.com>
2021-08-09 15:46:48 +0530

    project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'components';
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
 }
