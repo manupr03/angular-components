b91bf7555a944e78cbed8a0ecba51fa76e7903e3
ManuDev03 <manuprajan03@gmail.com>
2021-08-10 19:38:21 +0530

    lifecycle hooks of component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,10 @@ onServerAdded(serverData:{serverName:string,serverContent:string}){
     })
 
   }
+  onChangeFirst(){
+    this.serverElements[0].name = 'Changed!'
+  }
+  onDestroyFirst(){
+    this.serverElements.splice(0,1)
+  }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit,  SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,11 +6,44 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges,
+ DoCheck, AfterContentChecked, 
+ AfterContentInit, AfterViewChecked,AfterViewInit, OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string};
-  constructor() { }
+  @Input() name:string
+  
+  constructor() {
+    console.log('constructor called')
+   }
 
+   ngOnChanges(changes: SimpleChanges){
+    console.log('ngonChanges called')
+    console.log(changes)
+
+   }
   ngOnInit(): void {
+    console.log('ngonInit called')
+
   }
+  ngDoCheck(){
+    console.log('ngDocheck called ')
+  }
+  ngAfterContentChecked(){
+    console.log('ngAfterContentchecked called')
+  }
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called')
+  }
+ ngAfterViewChecked(){
+   console.log('ngAfterviewChecked called')
+ }
+ ngAfterViewInit(){
+  console.log('ngAfterviewInit called')
+}
+
+ngOnDestroy(){
+  
+}
+
 
 }
